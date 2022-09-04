7fd841dfc88e2575bef7981473f2ad16bd8b1726
sonamverma28045@gmail.com <sonamverma28045@gmail.com>
2022-09-04 12:08:09 +0530

    PowerSet

---
+++ Powerset.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		   int n=s.length();
+		   vector<string>ans;
+      	for (int num = 0; num <= (1 << n)-1; num++) {
+		string sub = "";
+		for (int i = 0; i <= n-1; i++) {
+			if (num & (1 << i)) {
+				sub += s[i];
+			}
+		}
+		if (sub.length() > 0) {
+			ans.push_back(sub);
+		}
+	}
+	sort(ans.begin(), ans.end());
+	return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
